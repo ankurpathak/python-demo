f917e6bec3a5ab21249c3660977361805513577c
Ankur Pathak <adpathak@lentra.ai>
2020-06-20 13:59:45 +0530

    Input From User

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
-full_name = 'John Smith'  # string
-age = 10  # integer
-is_new = True  # bool
-weight = 43.6  # float
-print("full_name: " + full_name)
-print("age: " + str(10))
-print("is_new: " + str(True))
-print("is_new: " + str(weight))
+name = input('Enter your name: ')
+color = input('Enter your favourite color: ')
+print(name + ' likes ' + color)
