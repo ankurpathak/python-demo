db668bd3fb495afa8123db4c2a0905464b39b7e9
Ankur Pathak <adpathak@lentra.ai>
2020-06-20 14:14:12 +0530

    type conversion

--- app.py
+++ app.py
@@ -1,3 +1,3 @@
-name = input('Enter your name: ')
-color = input('Enter your favourite color: ')
-print(name + ' likes ' + color)
+year = input('Enter year of birth: ')
+age = 2020 - int(year)
+print('Your age is ' + str(age))
