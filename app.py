bd7902c2e40864307e4285cc076127ed93c07a84
Ankur Pathak <adpathak@lentra.ai>
2020-06-20 13:34:53 +0530

    data types

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print('Ankur Pathak')
-print('O----')
-print(' ||||')
-print('*' * 10)
+full_name = 'John Smith'  # string
+age = 10  # integer
+is_new = True  # bool
+weight = 43.6  # float
+print("full_name: " + full_name)
+print("age: " + str(10))
+print("is_new: " + str(True))
+print("is_new: " + str(weight))
